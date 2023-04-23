8847221276dd23626a6b13da1d0ae9f449a63164
angrygun <horse190200240@126.com>
2023-04-23 17:36:48 +0800

    1.2 命令行学习/练习

---
+++ program_structure/variable/type/tempconv.go
@@ -0,0 +1,22 @@
+// Package tempconv performs Celsius and Fahrenheit temperature computation.
+package tempconv
+
+import "fmt"
+
+type Celsius float64 // 摄氏温度
+type Fahrenheit float64 // 华氏温度
+
+
+const (
+    AbsoluteZeroC Celsius = -273.15 // 绝对零度
+    FreezingC     Celsius = 0 // 结冰点温度
+    BoilingC      Celsius = 100 // 沸水温度
+)
+
+func (c Celsius) String() string {
+    return fmt.Sprintf("%g°C", c)
+}
+
+func (f Fahrenheit) String() string {
+    return fmt.Sprintf("%g°F", f)
+}
